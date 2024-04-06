437ee7a097ae5a001dd01ff6452147ab93252f7f
Everton Oliveira <tonn.oliveira2011@gmail.com>
2024-04-05 23:10:35 -0300

    Ajuiste na pasta de testes

---
+++ 02-Testes/Store.Domain/Entities/Entity.cs
@@ -0,0 +1,13 @@
+using Flunt.Notifications;
+
+namespace Store.Domain.Entities
+{
+    public class Entity: Notifiable
+  {
+    public Entity(){
+      Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+
+  }
+}
